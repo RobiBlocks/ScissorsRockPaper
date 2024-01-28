71617e1c18f9f8a717b53b6a651cd6aa5cf7bc44
RobiBlocks <142887221+RobiBlocks@users.noreply.github.com>
2024-01-28 14:52:21 +0100

    Add files via upload

---
+++ Version 3.0/Form1.cs
@@ -0,0 +1,159 @@
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace ScherSteinPapier
+{
+    public partial class Form1 : Form
+    {
+        int benutzerWahl;
+        int gegnerWahl;
+        int highscore = 0;
+        string highscoreString;
+
+        public Form1()
+        {
+            InitializeComponent();
+            string daten = Datenleser(GetPath());
+            string[] datenArray = daten.Split(',');
+
+            int nameStelle = datenArray.Length - 2;
+            labelPlayerName.Text = datenArray[nameStelle];
+        }
+
+        private void buttonSchere_Click(object sender, EventArgs e)
+        {
+            EingabeButtonsVerschwinden();
+            labelBenutzerWahl.Visible = true;
+            labelBenutzerWahl.Text = "Du hast Schere gewählt!";
+            GegnerWählt();
+            benutzerWahl = 1;
+            GewinnVergleich();
+            buttonBeenden.Visible = true;
+            buttonWeiter.Visible = true;
+        }
+
+        private void buttonStein_Click(object sender, EventArgs e)
+        {
+            EingabeButtonsVerschwinden();
+            labelBenutzerWahl.Visible = true;
+            labelBenutzerWahl.Text = "Du hast Stein gewählt!";
+            benutzerWahl = 2;
+            GegnerWählt();
+            GewinnVergleich();
+            buttonBeenden.Visible = true;
+            buttonWeiter.Visible = true;
+        }
+
+        private void buttonPapier_Click(object sender, EventArgs e)
+        {
+            EingabeButtonsVerschwinden();
+            labelBenutzerWahl.Visible = true;
+            labelBenutzerWahl.Text = "Du hast Papier gewählt!";
+            benutzerWahl = 3;
+            GegnerWählt();
+            GewinnVergleich();
+            buttonBeenden.Visible = true;
+            buttonWeiter.Visible = true;
+        }
+
+        void EingabeButtonsVerschwinden()
+        {
+            buttonSchere.Visible = false;
+            buttonStein.Visible = false;
+            buttonPapier.Visible = false;
+        }
+
+        void GegnerWählt()
+        {
+            Random rnd = new Random();
+            gegnerWahl = rnd.Next(1, 4); //1 = Schere, 2 = Stein, 3 = Papier
+            labelGegnerWahl.Visible = true;
+
+            if (gegnerWahl == 1)
+            {
+                labelGegnerWahl.Text = "Der Gegner hat Schere gewählt!";
+            }
+            if (gegnerWahl == 2)
+            {
+                labelGegnerWahl.Text = "Der Gegner hat Stein gewählt!";
+            }
+            if (gegnerWahl == 3)
+            {
+                labelGegnerWahl.Text = "Der Gegner hat Papier gewählt!";
+            }
+        }
+
+        void GewinnVergleich()
+        {
+            if (benutzerWahl == 1 && gegnerWahl == 1 ||
+                benutzerWahl == 2 && gegnerWahl == 2 ||
+                benutzerWahl == 3 && gegnerWahl == 3)
+            {
+                labelGewinner.Visible = true;
+                labelGewinner.Text = "UNENTSCHIEDEN!";
+            }
+            if (benutzerWahl == 1 && gegnerWahl == 3 ||
+                benutzerWahl == 2 && gegnerWahl == 1 ||
+                benutzerWahl == 3 && gegnerWahl == 2)
+            {
+                labelGewinner.Visible = true;
+                labelGewinner.Text = "Du hast GEWONNEN!";
+                highscore = highscore + 1;
+            }
+            if (benutzerWahl == 3 && gegnerWahl == 1 ||
+                benutzerWahl == 1 && gegnerWahl == 2 ||
+                benutzerWahl == 2 && gegnerWahl == 3)
+            {
+                labelGewinner.Visible = true;
+                labelGewinner.Text = "Du hast VERLOREN!";
+            }
+        }
+
+        private void buttonBeenden_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            FormStart frmStart = new FormStart();
+            frmStart.ShowDialog();
+        }
+
+        private void buttonWeiter_Click(object sender, EventArgs e)
+        {
+            int runde = Convert.ToInt32(labelRunde.Text);
+            labelRunde.Text = Convert.ToString(runde + 1);
+
+            buttonWeiter.Visible = false;
+            buttonSchere.Visible = true;
+            buttonStein.Visible = true;
+            buttonPapier.Visible = true;
+            labelBenutzerWahl.Visible = false;
+            labelGegnerWahl.Visible = false;
+            labelGewinner.Visible = false;
+        }
+        static string GetPath()
+        {
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "highscore.txt");
+        }
+        static string Datenleser(string dateiName)
+        {
+            if (File.Exists(dateiName))
+            {
+                return File.ReadAllText(dateiName);
+            }
+            return "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelRunde.Text = "1";
+        }
+
+        private void Form1_Closing(object sender, FormClosingEventArgs e)
+        {
+            highscoreString = highscore.ToString();
+            string daten = Datenleser(GetPath());
+            string inhalt = daten + highscoreString + ",";
+            string pfad = GetPath();
+            File.WriteAllText(pfad, inhalt);
+        }
+    }
+}
---
+++ Version 3.0/FormStart.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace ScherSteinPapier
+{
+    partial class FormStart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonStart = new Button();
+            buttonBeenden = new Button();
+            textBoxName = new TextBox();
+            labelScore = new Label();
+            labelName = new Label();
+            SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            buttonStart.Font = new Font("Trebuchet MS", 26.25F, FontStyle.Bold, GraphicsUnit.Point);
+            buttonStart.Location = new Point(225, 392);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(360, 180);
+            buttonStart.TabIndex = 0;
+            buttonStart.Text = "Start";
+            buttonStart.UseVisualStyleBackColor = true;
+            buttonStart.Click += buttonStart_Click;
+            // 
+            // buttonBeenden
+            // 
+            buttonBeenden.Font = new Font("Trebuchet MS", 26.25F, FontStyle.Bold, GraphicsUnit.Point);
+            buttonBeenden.Location = new Point(677, 392);
+            buttonBeenden.Name = "buttonBeenden";
+            buttonBeenden.Size = new Size(360, 180);
+            buttonBeenden.TabIndex = 1;
+            buttonBeenden.Text = "Beenden";
+            buttonBeenden.UseVisualStyleBackColor = true;
+            buttonBeenden.Click += buttonBeenden_Click;
+            // 
+            // textBoxName
+            // 
+            textBoxName.Location = new Point(545, 139);
+            textBoxName.Name = "textBoxName";
+            textBoxName.Size = new Size(292, 23);
+            textBoxName.TabIndex = 9;
+            // 
+            // labelScore
+            // 
+            labelScore.AutoSize = true;
+            labelScore.Location = new Point(447, 233);
+            labelScore.Name = "labelScore";
+            labelScore.Size = new Size(40, 15);
+            labelScore.TabIndex = 10;
+            labelScore.Text = "scores";
+            // 
+            // labelName
+            // 
+            labelName.AutoSize = true;
+            labelName.Font = new Font("Trebuchet MS", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            labelName.Location = new Point(447, 132);
+            labelName.Name = "labelName";
+            labelName.Size = new Size(93, 35);
+            labelName.TabIndex = 11;
+            labelName.Text = "Name:";
+            // 
+            // FormStart
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1264, 681);
+            Controls.Add(labelName);
+            Controls.Add(labelScore);
+            Controls.Add(textBoxName);
+            Controls.Add(buttonBeenden);
+            Controls.Add(buttonStart);
+            Name = "FormStart";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "FormStart";
+            Load += FormStart_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonStart;
+        private Button buttonBeenden;
+        private TextBox textBoxName;
+        private Label labelScore;
+        private Label labelName;
+    }
+}
